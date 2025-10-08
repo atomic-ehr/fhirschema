8d9597a3300d28c8a32baeb2ecc71fd01ccb4ccd
Guillermo <guijealth@gmail.com>
2025-10-08 11:35:29 +0200

    adding test for profiles chain merge

--- test/converter/slicing.test.ts
+++ test/converter/slicing.test.ts
@@ -1,7 +1,26 @@
 import { describe, test, expect } from 'bun:test';
 import * as sut from '../../src/converter/slicing';
+import { FHIRSchema } from '../../src/converter/types';
+import usCoreBloodPressureProfile from '../data/hl7.fhir.us.core#8.0.0-ballot/us-core-blood-pressure.fs.json';
+import usCoreVitalSignsProfile from '../data/hl7.fhir.us.core#8.0.0-ballot/us-core-vital-signs.fs.json';
+import r4VitalSignsProfile from '../data/hl7.fhir.r4.core#4.0.1/vitalsigns.fs.json';
+import r4ObsProfile from '../data/hl7.fhir.r4.core#4.0.1/Observation.fs.json';
+import r4DomainResProfile from '../data/hl7.fhir.r4.core#4.0.1/DomainResource.fs.json';
+import r4ResourceProfile from '../data/hl7.fhir.r4.core#4.0.1/Resource.fs.json';
 import slicingObsComponent from '../data/slicing-obs-component.json';
 import reslicingPatPassport from '../data/reslicing-patient-passport.json';
+import usCoreBloodPreasureProfilesChain from '../data/uscore-blood-preasure-profiles-chain.json';
+
+const profilesIndex: {[key in string]: FHIRSchema} = {
+  'http://hl7.org/fhir/us/core/StructureDefinition/us-core-blood-pressure|8.0.0-ballot':
+    usCoreBloodPressureProfile,
+  'http://hl7.org/fhir/us/core/StructureDefinition/us-core-vital-signs|8.0.0-ballot':
+    usCoreVitalSignsProfile,
+  'http://hl7.org/fhir/StructureDefinition/vitalsigns|4.0.1': r4VitalSignsProfile,
+  'http://hl7.org/fhir/StructureDefinition/Observation|4.0.1': r4ObsProfile,
+  'http://hl7.org/fhir/StructureDefinition/DomainResource|4.0.1': r4DomainResProfile,
+  'http://hl7.org/fhir/StructureDefinition/Resource|4.0.1': r4ResourceProfile,
+};
 
 describe('Slicing merge', () => {
   describe('Observation.component at us-core vital-signs', () => {
@@ -16,6 +35,15 @@ describe('Slicing merge', () => {
       const result = sut.merge(reslicingPatPassport.base, reslicingPatPassport.overlay);
 
       expect(result).toEqual(reslicingPatPassport.result);
+    });
+  });
+  describe('Chain of schemas', () => {
+    test('us-core-blood-pressure at hl7.fhir.us.core#8.0.0-ballot', () => {
+      const result = usCoreBloodPreasureProfilesChain.profiles
+        .map((canon) => profilesIndex[canon])
+        .reduce((p1, p2) => sut.merge(p1, p2) as FHIRSchema);
+
+      expect(result).toEqual(usCoreBloodPreasureProfilesChain.result);
     });
   });
 });
