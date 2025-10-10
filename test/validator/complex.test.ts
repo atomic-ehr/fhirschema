d2426114c31f633a496afe058821965af254c446
Guillermo <guijealth@gmail.com>
2025-10-10 18:19:07 +0200

    adding negative complex-type test: coding-bad-extra-field

--- test/validator/complex.test.ts
+++ test/validator/complex.test.ts
@@ -3,14 +3,21 @@ import { OperationOutcome } from '../../src/converter/types';
 import { typesIndex } from './fixture';
 import * as sut from '../../src/validator/complex';
 import codingGood1 from '../data/coding-good-1.json';
+import codingBadExtraField from '../data/coding-bad-extra-field.json';
 
 describe('Complex-type validations', () => {
   describe('Single level types', () => {
-    test('US core blood preasure component', () => {
+    test('Coding good', () => {
       const spec = typesIndex[codingGood1.type];
       const result = sut.validate(codingGood1.value, spec, [], typesIndex);
 
       expect(result).toEqual(codingGood1.result as OperationOutcome);
+    });
+    test('Coding extra field', () => {
+      const spec = typesIndex[codingBadExtraField.type];
+      const result = sut.validate(codingBadExtraField.value, spec, [], typesIndex);
+
+      expect(result).toEqual(codingBadExtraField.result as OperationOutcome);
     });
   });
   describe('Multi level types', () => {});
